1b9ac11bb08da00dcd73628e7c9d50fdaa6bb3d7
Dixter <artem.ryzhenkov.2004@mail.ru>
2022-01-17 23:12:33 +0300

    Добавьте файлы проекта.

---
+++ BigBlueButtonPresentationParser/LoadingBar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBlueButtonPresentationParser
+{
+    internal class LoadingBar
+    {
+        private double _currentValue;
+        private int _topOffset;
+
+        private double _minValue;
+        private double _maxValue;
+        private int _segments;
+
+        private List<object[]> _colorParts = new List<object[]>();
+
+        public LoadingBar(double minValue, double maxValue, int segments = 10, ConsoleColor defaultColor = ConsoleColor.Gray)
+        {
+            _currentValue = minValue;
+            _minValue = minValue;
+            _maxValue = maxValue;
+            _segments = segments;
+
+            _topOffset = Console.CursorTop;
+            
+            if(defaultColor != ConsoleColor.Gray)
+                _colorParts.Add(new object[] { 0, defaultColor });
+
+            Render();
+        }
+
+        public void Update(double newValue)
+        {
+            _currentValue = newValue;
+            Render();
+        }
+
+        public double GetValue()
+        {
+            return _currentValue;
+        }
+
+        public void SetMaxValue(double newValue)
+        {
+            if (newValue > _minValue)
+                _maxValue = newValue;
+        }
+
+        public void AddColor(ConsoleColor color)
+        {
+            double t = MathExtension.InverseLerp(_minValue, _maxValue, _currentValue);
+            int count = (int)MathExtension.Lerp(0, _segments, t);
+
+            _colorParts.Add(new object[] { count, color });
+        }
+
+        private void Render()
+        {
+            int[] offset = new int[2] { Console.CursorLeft, Console.CursorTop };
+
+            Write(new string(' ', _segments + 8), 0, 0);
+            Write("[", 0, 0);
+            Write("]", _segments + 1, 1);
+
+            double t = MathExtension.InverseLerp(_minValue, _maxValue, _currentValue);
+            int count = (int)MathExtension.Lerp(0, _segments, t);
+
+            Write($"{(int)(t * 100)}%", _segments + 3, 1);
+            Write(new string('=', _colorParts.Count == 0 ? count : (int)_colorParts[0][0]), 1, 1);
+
+            for(int i = 0; i < _colorParts.Count; i++)
+            {
+                int startIndex = (int)_colorParts[i][0];
+                ConsoleColor color = (ConsoleColor)_colorParts[i][1];
+                int length = 0;
+
+                if (i < _colorParts.Count - 1)
+                    length = (int)_colorParts[i + 1][0];
+                else
+                    length = count - startIndex;
+
+                Console.ForegroundColor = color;
+                Write(new string('=', length), 1 + startIndex, 1);
+            }
+            
+
+            Console.SetCursorPosition(offset[0], offset[1]);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private void Write(string value, int start, int end)
+        {
+            Console.SetCursorPosition(start, _topOffset);
+            Console.Write(value);
+            Console.SetCursorPosition(end, _topOffset);
+        }
+    }
+}
---
+++ BigBlueButtonPresentationParser/Program.cs
@@ -0,0 +1,149 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigBlueButtonPresentationParser
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.Write("Введите базовую ссылку: ");
+            string url = Console.ReadLine();
+
+            WriteLine("Запускаем виртуальный браузер...", ConsoleColor.Yellow);
+            Console.CursorVisible = false;
+
+            string tempPdfFile = ParseAndGeneratePresentation(url);
+            WriteLine("Выберите место для сохранения PDF-файла", ConsoleColor.Yellow);
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.FileName = tempPdfFile;
+            saveDialog.Filter = "PDF-Файлы|*.pdf";
+            saveDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                MoveFileAsync(tempPdfFile, saveDialog.FileName);
+                WriteLine("PDF-файл сохранён", ConsoleColor.Yellow);
+            }
+            else
+            {
+                WriteLine("Сохранение файла отменено", ConsoleColor.Yellow);
+            }
+
+            Console.WriteLine("Нажмите Enter для выхода...");
+            Console.ReadLine();
+            Environment.Exit(0);
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private static async void MoveFileAsync(string source, string destination)
+        {
+            await Task.Run(() => File.Move(source, destination));
+        }
+
+        private static string ParseAndGeneratePresentation(string url)
+        {
+            EdgeOptions otis = new EdgeOptions();
+            otis.AddArgument("--headless");
+            var browser = new EdgeDriver(otis);
+
+            browser.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+            var scrs = new List<string>();
+
+            int count = 0;
+            WriteLine("Получаем количество слайдов...", ConsoleColor.Yellow);
+            for (int i = 1; i < 100; i++)
+            {
+                try
+                {
+                    browser.Url = $@"{url}{i}";
+                    if (browser.PageSource.Contains("404 Not Found"))
+                        break;
+
+                    count++;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            WriteLine("Количество слайдов: " + count, ConsoleColor.Yellow);
+            WriteLine("Началось скачивание слайдов...", ConsoleColor.Yellow);
+            LoadingBar downloadLoadingBar = new LoadingBar(1, count - 1, 50, ConsoleColor.Red);
+            Console.WriteLine();
+            for (int i = 1; i < count; i++)
+            {
+                try
+                {
+                    browser.Url = $@"{url}{i}";
+                    if (browser.PageSource.Contains("404 Not Found"))
+                        break;
+
+                    int width = browser.FindElement(By.Id("surface1")).Size.Width;
+                    int height = browser.FindElement(By.Id("surface1")).Size.Height;
+                    browser.Manage().Window.Size = new System.Drawing.Size(width + 10, height + 50);
+                    var scr = browser.GetScreenshot();
+                    string imgPath = $"{DateTime.Now.Ticks + i}.jpg";
+                    scr.SaveAsFile(imgPath);
+                    scrs.Add(imgPath);
+                    downloadLoadingBar.Update(i);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            browser.Close();
+            Thread.Sleep(1);
+
+            if (scrs.Count() == 0)
+                return null;
+
+            WriteLine("Объединяем слайды в PDF-файл...", ConsoleColor.Yellow);
+            LoadingBar savingLoadingBar = new LoadingBar(1, count, 50, ConsoleColor.Cyan);
+            Console.WriteLine();
+
+            Image im = Image.GetInstance(scrs[0]);
+            var doc = new Document(new Rectangle(im));
+            string filename = $"{DateTime.Now.Ticks}.pdf";
+            PdfWriter.GetInstance(doc, new FileStream(filename, FileMode.Create));
+            doc.Open();
+
+            Thread.Sleep(1);
+            foreach (string img in scrs)
+            {
+                Image i = Image.GetInstance(img);
+                doc.SetPageSize(new Rectangle(i));
+                i.Alignment = Element.ALIGN_CENTER;
+                i.ScaleToFit(doc.PageSize.Width, doc.PageSize.Height);
+                doc.Add(i);
+                doc.NewPage();
+                File.Delete(img);
+                savingLoadingBar.Update(savingLoadingBar.GetValue() + 1);
+            }
+            doc.Close();
+            return filename;
+        }
+
+        private static void WriteLine(string text, ConsoleColor color)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = original;
+        }
+    }
+}
