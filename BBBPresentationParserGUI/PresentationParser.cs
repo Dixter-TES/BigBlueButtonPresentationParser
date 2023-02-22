f97c6232a73213689bd3811a1cd1f28f6fa9341f
Dixter-TES <artem.ryzhenkov.2004@mail.ru>
2023-02-23 02:39:11 +0300

    presentation & usings refactor

--- BBBPresentationParserGUI/PresentationParser.cs
+++ BBBPresentationParserGUI/PresentationParser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,6 +13,14 @@ namespace BBBPresentationParser
         private IBrowser _browser;
         private readonly string _baseUrl;
 
+        private const int MaxSlidesCount = 200;
+        private const string SlidesEndIndicator = "404 Not Found";
+        private readonly string[] TempFileFormats = new string[]
+        {
+            "*.jpg",
+            "*.pdf"
+        };
+
         public EventHandler<string>? SlideParsed;
 
         public PresentationParser(string baseUrl, IBrowser driver)
@@ -21,11 +30,11 @@ namespace BBBPresentationParser
 
             try
             {
-                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jpg"))
-                    File.Delete(file);
-
-                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.pdf"))
-                    File.Delete(file);
+                foreach(var format in TempFileFormats)
+                {
+                    foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), format))
+                        File.Delete(file);
+                }
             }
             catch { }
         }
@@ -44,14 +53,14 @@ namespace BBBPresentationParser
             var page = await _browser.NewPageAsync();
             var screenshots = new List<string>();
 
-            for (int i = 1; i < 200; i++)
+            for (int i = 1; i < MaxSlidesCount; i++)
             {
                 try
                 {
                     await page.GoToAsync($@"{_baseUrl}{i}");
 
                     string content = await page.GetContentAsync();
-                    if (content.Contains("404 Not Found"))
+                    if (content.Contains(SlidesEndIndicator))
                         break;
 
                     var svgElement = await page.QuerySelectorAsync("svg");
